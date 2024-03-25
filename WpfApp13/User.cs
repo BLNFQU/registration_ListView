e19d1e7ef9e435c035945e782a08f7a955e2c189
Кирилл <@DESKTOP-MPM09EH>
2024-03-25 23:01:45 +0500

    Добавьте файлы проекта.

---
+++ WpfApp13/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp13
+{
+    internal class User
+    {
+        static public LinkedList<User> users = new LinkedList<User>();
+        string Name { get; set; }
+        string SubName { get; set; }
+        string Password { get; set; }
+        string SubPass { get; set; }
+        public User(string _name, string _subName, string _password, string _subPass) 
+        {
+            Name = _name;
+            SubName = _subName;
+            Password = _password;
+            SubPass = _subPass;
+        }
+    }
+}
